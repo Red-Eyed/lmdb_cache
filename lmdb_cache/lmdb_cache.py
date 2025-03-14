491b5ec0aa89f4b967309eb9fac5fa16fa2f0dca
Vadym Stupakov <vadim.stupakov@gmail.com>
2025-03-14 09:27:35 +0200

    refactored

--- lmdb_cache/lmdb_cache.py
+++ lmdb_cache/lmdb_cache.py
@@ -8,17 +8,7 @@ from typing import Any, Iterable
 import lmdb
 from pathlib import Path
 import dill
-
-
-def _serialize(obj) -> bytes:
-    data = dill.dumps(obj, protocol=dill.HIGHEST_PROTOCOL)
-    return data
-
-
-def _deserialize(data: bytes) -> Any:
-    obj = dill.loads(data)
-    return obj
-
+import brotli
 
 def lmdb_exists(p: Path) -> bool:
     if p.exists():
@@ -32,7 +22,27 @@ def lmdb_exists(p: Path) -> bool:
         return False
 
 
-class LMDBReadDict:
+class SerializeMixIn:
+    @classmethod
+    def serialize(cls, obj) -> bytes:
+        data = dill.dumps(obj, protocol=dill.HIGHEST_PROTOCOL)
+        return data
+
+    @classmethod
+    def deserialize(cls, data: bytes) -> Any:
+        obj = dill.loads(data)
+        return obj
+
+    @classmethod
+    def get_data(cls, key: int, value: Any):
+        return str(key).encode(), cls.serialize(value)
+
+    @classmethod
+    def get_size(cls, key: bytes, value: bytes):
+        return len(key) + len(value)
+
+
+class LMDBCache(SerializeMixIn):
     def __init__(self, db_path: Path) -> None:
         """
         db_path: valid path which points to a directory which contains "data.mdb" and "lock.mdb" files
@@ -71,47 +81,69 @@ class LMDBReadDict:
 
     def __getitem__(self, index):
         lmdb_data = self._txn().get(str(index).encode())
-        data = _deserialize(lmdb_data)
+        if lmdb_data is None:
+            raise KeyError(index)
+        else:
+            data = self.deserialize(lmdb_data)
 
         return data
 
+    @classmethod
+    def from_iterable(
+        cls, db_path: Path, iterable: Iterable, size_multiplier=100, block_size=1024**2
+    ) -> Path:
+        db_path.mkdir(parents=True)
+        all_size = 0
+        open_lmdb = partial(
+            lmdb.open,
+            path=db_path.as_posix(),
+            subdir=True,
+            metasync=False,
+            sync=False,
+            writemap=True,
+            map_async=True,
+        )
+        try:
+            env = open_lmdb()
+            for i, value in enumerate(iterable):
+                key, value = cls.get_data(i, value)
+                size = cls.get_size(key, value)
+                all_size += size
 
-def _get_data(key: int, value: Any):
-    return str(key).encode(), _serialize(value)
+                try:
+                    with env.begin(write=True) as txn:
+                        txn.put(key, value)
+                except lmdb.MapFullError:
+                    # when LMDB reaches max size: close it, then reopenen with increased size
+                    all_size += max(block_size, size) * size_multiplier
+                    env.close()
+
+                    env = open_lmdb(map_size=all_size)
+                    with env.begin(write=True) as txn:
+                        txn.put(key, value)
+
+            env.close()
+        except Exception:
+            shutil.rmtree(db_path)
+            raise
+
+        assert lmdb_exists(db_path)
+
+        return db_path
+
+class SerializeWithCompressionMixIn(SerializeMixIn):
+    @classmethod
+    def serialize(cls, obj) -> bytes:
+        data = super().serialize(obj)
+        data = brotli.compress(data)
+        return data
+
+    @classmethod
+    def deserialize(cls, data: bytes) -> Any:
+        data = brotli.decompress(data)
+        data = super().deserialize(data)
+        return data
 
 
-def _get_size(key: bytes, value: bytes):
-    return len(key) + len(value)
-
-
-def dump2lmdb(db_path: Path, iterable: Iterable, size_multiplier=100, block_size=1024**2) -> Path:
-    db_path.mkdir(parents=True)
-    all_size = 0
-    open_lmdb = partial(lmdb.open, path=db_path.as_posix(), subdir=True, metasync=False, sync=False, writemap=True, map_async=True)
-    try:
-        env = open_lmdb()
-        for i, value in enumerate(iterable):
-            key, value = _get_data(i, value)
-            size = _get_size(key, value)
-            all_size += size
-
-            try:
-                with env.begin(write=True) as txn:
-                    txn.put(key, value)
-            except lmdb.MapFullError:
-                # when LMDB reaches max size: close it, then reopenen with increased size
-                all_size += max(block_size, size) * size_multiplier
-                env.close()
-
-                env = open_lmdb(map_size=all_size)
-                with env.begin(write=True) as txn:
-                    txn.put(key, value)
-
-        env.close()
-    except Exception:
-        shutil.rmtree(db_path)
-        raise
-
-    assert lmdb_exists(db_path)
-
-    return db_path
+class LMDBCacheCompressed(LMDBCache, SerializeWithCompressionMixIn):
+    pass
--- tests/test_lmdb_tools.py
+++ tests/test_lmdb_tools.py
@@ -1,19 +1,26 @@
 from pathlib import Path
 import pytest
 from multiprocessing import Pool, Process, Value
-from lmdb_cache.lmdb_cache import _serialize, _deserialize, lmdb_exists, LMDBReadDict, dump2lmdb
+from lmdb_cache.lmdb_cache import LMDBCache, LMDBCacheCompressed, lmdb_exists
+
+class_under_test_t = LMDBCache | LMDBCacheCompressed
+
+
+@pytest.fixture(params=[LMDBCache, LMDBCacheCompressed])
+def class_under_test(request):
+    return request.param
 
 @pytest.fixture
-def setup_lmdb(tmp_path):
-    db_path = tmp_path / "lmdb_test"
+def setup_lmdb(request, class_under_test: class_under_test_t, tmp_path):
+    db_path = tmp_path / f"{request.node.name}_lmdb_test"
     data = {i: f"data_{i}" for i in range(100)}
-    dump2lmdb(db_path, data.values())
+    class_under_test.from_iterable(db_path, data.values())
     return db_path, data
 
-def test_serialize_deserialize():
+def test_serialize_deserialize(class_under_test):
     obj = {'a': 1, 'b': 2}
-    serialized_obj = _serialize(obj)
-    deserialized_obj = _deserialize(serialized_obj)
+    serialized_obj = class_under_test.serialize(obj)
+    deserialized_obj = class_under_test.deserialize(serialized_obj)
     assert obj == deserialized_obj
 
 def test_lmdb_exists(tmp_path):
@@ -30,42 +37,44 @@ def test_lmdb_exists_not_a_directory(tmp_path):
     with pytest.raises(NotADirectoryError):
         lmdb_exists(db_file)
 
-def test_LMDBReadDict_init(setup_lmdb):
+def test_init(class_under_test: class_under_test_t, setup_lmdb):
     db_path, _ = setup_lmdb
-    db = LMDBReadDict(db_path)
-    assert isinstance(db, LMDBReadDict)
+    db = class_under_test(db_path)
+    assert isinstance(db, class_under_test)
 
-def test_LMDBReadDict_len(setup_lmdb):
+def test_len(class_under_test: class_under_test_t, setup_lmdb):
     db_path, data = setup_lmdb
-    db = LMDBReadDict(db_path)
+    db = class_under_test(db_path)
     assert len(db) == len(data)
 
-def test_LMDBReadDict_getitem(setup_lmdb):
+def test_getitem(class_under_test, setup_lmdb):
     db_path, data = setup_lmdb
-    db = LMDBReadDict(db_path)
+    db = class_under_test(db_path)
     for key, value in data.items():
         assert db[key] == value
 
-def test_dump2lmdb(tmp_path):
+def test_from_iterable(class_under_test, tmp_path):
     db_path = tmp_path / "lmdb_dump"
     data = [f"data_{i}" for i in range(10)]
-    dump2lmdb(db_path, data)
+    class_under_test.from_iterable(db_path, data)
     assert lmdb_exists(db_path)
 
-    db = LMDBReadDict(db_path)
+    db = class_under_test(db_path)
     for i, value in enumerate(data):
         assert db[i] == value
 
-def test_dump2lmdb_cleanup_on_failure(tmp_path):
+def test_from_iterable_cleanup_on_failure(tmp_path):
     db_path = tmp_path / "lmdb_fail"
     with pytest.raises(Exception):
-        dump2lmdb(db_path, iter(Exception('Forced failure')), size_multiplier=1, block_size=1)
+        class_under_test.from_iterable(
+            db_path, iter(Exception("Forced failure")), size_multiplier=1, block_size=1
+        )
     assert not db_path.exists()
 
 
-def test_concurrent_reading(setup_lmdb):
+def test_concurrent_reading(class_under_test, setup_lmdb):
     db_path, data = setup_lmdb
-    db = LMDBReadDict(db_path)
+    db = class_under_test(db_path)
     with Pool(5) as p:
         keys = list(data.keys())
         results = p.map(db.__getitem__, keys)
@@ -74,24 +83,25 @@ def test_concurrent_reading(setup_lmdb):
     for key, result in zip(keys, results):
         assert data[key] == result
 
-def test_LMDBReadDict_invalid_path():
+def test_invalid_path(class_under_test):
     with pytest.raises(RuntimeError):  # Assuming RuntimeError for non-existent DB
-        LMDBReadDict(Path("/path/to/nonexistent/db"))
+        class_under_test(Path("/path/to/nonexistent/db"))
 
-def test_LMDBReadDict_nonexistent_key(setup_lmdb):
+def test_nonexistent_key(class_under_test, setup_lmdb):
     db_path, _ = setup_lmdb
-    db = LMDBReadDict(db_path)
-    with pytest.raises(EOFError):  # Assuming KeyError for non-existent key
+    db = class_under_test(db_path)
+    with pytest.raises(KeyError):  # Assuming KeyError for non-existent key
         _ = db['nonexistent_key']
 
-def test_LMDBReadDict_invalid_db_path_type():
+
+def test_invalid_db_path_type(class_under_test):
     with pytest.raises(AttributeError):  # Adjust the exception type as needed
-        LMDBReadDict(12345)  # Passing an integer instead of a Path or string
+        class_under_test(12345)  # Passing an integer instead of a Path or string
 
 
 def attempt_write(db_path, success_flag):
     try:
-        db = LMDBReadDict(db_path)  # Assuming this opens the DB in read-only mode
+        db = class_under_test(db_path)  # Assuming this opens the DB in read-only mode
         with db._env.begin(write=True) as txn:  # Attempt to start a write transaction
             txn.put(b'key', b'value')
         success_flag.value = 1
